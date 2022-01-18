df939576c189eac906e05d60df76315babd3dca4
ivan271325 <ivn.ma41666@cesurformacion.com>
2022-01-18 21:03:09 +0100

    Update App.js
    separo el proyecto por carpetas dejando el app.js sin funciones ni estilos ni imagenes

--- App.js
+++ App.js
@@ -1,233 +1,147 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View, Image, Alert, StyleSheet, SafeAreaView, FlatList, Button, ScrollView, TextInput, RefreshControl } from 'react-native';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import ModalDropdown from 'react-native-modal-dropdown';
-
-const HomeStack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+import ListadoPantalla from './src/container/ListadoPantalla';
+import InformacionPantalla from './src/container/InformacionPantalla';
 
-//estilos de las cosas
-const styles = StyleSheet.create(
-  {
-    informacion: {
-      textAlign: 'center',
-      padding: 30,
-      fontWeight: 'bold',
-      fontSize: 20,
-      color: 'black',
-    },
-    busqueda: {
-      flexDirection: 'row',
-      padding:30,
-    },
-    frutas: {
-      flex: 2,
-      textAlign: 'center',
-      padding: 10,
-      fontSize: 20,
-      color: 'black',
-    },
-    textofrutas: {
-      fontFamily: 'Gill Sans Extrabold',
-      fontWeight: 'bold',
-      fontSize: 17,
-      marginLeft: 80,
-      textAlign: "center",
-      color:'black',
-    },
-    textocentro: {
-      textAlign: "center",
-      marginLeft: 80,
-      marginTop:20,
-      fontSize: 15,
-      color: 'black'
-    },
-    preciofrutas: {
-      fontFamily: 'Comic',
-      color: 'red',
-    },
-    opciones: {
-      textAlign: 'center',
-      fontWeight: 'bold',
-      fontSize: 20,
-      color: 'black',
-      marginHorizontal: 50
-    },
-    select: {
-      flex: 3,
-      textAlign: 'center',
-      fontWeight: 'bold',
-      fontSize: 20,
-      color: 'black',
-      borderWidth: 1,
-    },
-    input: {
-      flex: 3,
-      textAlign: 'center',
-      fontWeight: 'bold',
-      fontSize: 20,
-      color: 'black',
-      borderWidth: 1,
-    },
-    view: {
-      flexDirection: 'row',
-      marginLeft: 10,
-      marginBottom: 10,
-      borderBottomColor: 'blue',
-      borderBottomWidth: 1,
-      borderBottomStartRadius: 3,
-      textAlign: "center",
-    },
-    frutaLogo: {
-      marginRight: 5,
-      width: 80,
-      height: 80,
-    }
-  }
-)
+// function InformacionPantalla() {
+//   const [fruits, setFruits] = useState(null);
+//   const [refreshing, setRefreshing] = useState(false); 
+//   const [loading, setLoading] =  useState(true);
 
-function InformacionPantalla() {
-  const [fruits, setFruits] = useState(null);
-  const [refreshing, setRefreshing] = useState(false); 
-  const [loading, setLoading] =  useState(true);
+//   const wait = (timeout) => {
+//     return new Promise(resolve => {
+//       setTimeout(resolve, timeout);
+//     });
+//   }
 
-  const wait = (timeout) => {
-    return new Promise(resolve => {
-      setTimeout(resolve, timeout);
-    });
-  }
+//   const onRefresh = React.useCallback(() => {
+//     setRefreshing(true);
+//     wait(2000).then(() => setRefreshing(false), getFruits());
+//   }, []);
 
-  const onRefresh = React.useCallback(() => {
-    setRefreshing(true);
-    wait(2000).then(() => setRefreshing(false), getFruits());
-  }, []);
+//   function getFruits() {
+//     fetch("http://10.0.2.2:8080/fruits")
+//       .then(response => response.json())
+//       .then((responseJson) => {
+//         console.log('getting data from fectch', responseJson);
+//         setFruits(responseJson);
+//         setLoading(false);
+//       })
+//       .catch(error => console.log(error))
+//   }
 
-  function getFruits() {
-    fetch("http://10.0.2.2:8080/fruits")
-      .then(response => response.json())
-      .then((responseJson) => {
-        console.log('getting data from fectch', responseJson);
-        setFruits(responseJson);
-        setLoading(false);
-      })
-      .catch(error => console.log(error))
-  }
+//   useEffect(() => {
+//     getFruits();
+//   }, [])
 
-  useEffect(() => {
-    getFruits();
-  }, [])
+//   function imagenFruta(item){
+//     if('Kiwi'===item.name)
+//     return <Image style={styles.frutaLogo} source={ require('./src/imagenes/kiwi.png')}/>
+//     else if ('Manzana'===item.name)
+//     return <Image style={styles.frutaLogo} source={ require('./src/imagenes/manzana.png')}/>
+//     else if ('Melocotón'===item.name)
+//     return <Image style={styles.frutaLogo} source={ require('./src/imagenes/melocoton.png')}/>
+//     else if ('Naranja'===item.name)
+//     return <Image style={styles.frutaLogo} source={ require('./src/imagenes/naranja.png')}/>
+//     else if ('Piña'===item.name)
+//     return <Image style={styles.frutaLogo} source={ require('./src/imagenes/piña.png')}/>
+//     else if ('Platano'===item.name)
+//     return <Image style={styles.frutaLogo} source={ require('./src/imagenes/platano.png')}/>
+//     else if ('Uvas'===item.name)
+//     return <Image style={styles.frutaLogo} source={ require('./src/imagenes/uva.png')}/>
+//   }
 
-  function imagenFruta(item){
-    if('Kiwi'===item.name)
-    return <Image style={styles.frutaLogo} source={ require('./src/imagenes/kiwi.png')}/>
-    else if ('Manzana'===item.name)
-    return <Image style={styles.frutaLogo} source={ require('./src/imagenes/manzana.png')}/>
-    else if ('Melocotón'===item.name)
-    return <Image style={styles.frutaLogo} source={ require('./src/imagenes/melocoton.png')}/>
-    else if ('Naranja'===item.name)
-    return <Image style={styles.frutaLogo} source={ require('./src/imagenes/naranja.png')}/>
-    else if ('Piña'===item.name)
-    return <Image style={styles.frutaLogo} source={ require('./src/imagenes/piña.png')}/>
-    else if ('Platano'===item.name)
-    return <Image style={styles.frutaLogo} source={ require('./src/imagenes/platano.png')}/>
-    else if ('Uvas'===item.name)
-    return <Image style={styles.frutaLogo} source={ require('./src/imagenes/uva.png')}/>
-  }
-
-  const renderizarItem = ({ item }) => (
+//   const renderizarItem = ({ item }) => (
     
-    <View style={styles.view}>
-    {imagenFruta(item)}
-      <View>
-        <Text style={styles.textofrutas}>
-          {item.name}
-        </Text>
-        <Text style={styles.textocentro}>
-          Precio: 
-          <Text> </Text>
-          <Text style={styles.preciofrutas}>
-            {item.price}€ 
-          </Text>
-        </Text>
-      </View>
-    </View>
-  );
+//     <View style={styles.view}>
+//     {imagenFruta(item)}
+//       <View>
+//         <Text style={styles.textofrutas}>
+//           {item.name}
+//         </Text>
+//         <Text style={styles.textocentro}>
+//           Precio: 
+//           <Text> </Text>
+//           <Text style={styles.preciofrutas}>
+//             {item.price}€ 
+//           </Text>
+//         </Text>
+//       </View>
+//     </View>
+//   );
 
-  if(loading){
-    return (<Text>Cargando...</Text>)
-  }
-  else{
-    return (
-      <ScrollView 
-        refreshControl={
-        <RefreshControl 
-          refreshing={refreshing}
-          onRefresh={onRefresh}
-        />
-      }>
-        <Text> </Text>
-        <FlatList
-          data={fruits}
-          renderItem={renderizarItem}
-          keyExtractor={item=>item.id}
-        />
-      </ScrollView> 
-    );
-  }
-  
-}
+//   if(loading){
+//     return (<Text>Cargando...</Text>)
+//   }
+//   else{
+//     return (
+//       <ScrollView 
+//         refreshControl={
+//         <RefreshControl 
+//           refreshing={refreshing}
+//           onRefresh={onRefresh}
+//         />
+//       }>
+//         <Text> </Text>
+//         <FlatList
+//           data={fruits}
+//           renderItem={renderizarItem}
+//           keyExtractor={item=>item.id}
+//         />
+//       </ScrollView> 
+//     );
+//   }
+// }
 
-function ListadoPantalla() {
+// function ListadoPantalla() {
 
-  const [fruit,setFruit]=useState(null);
-  const [price,setPrice]=useState(null);
+//   const [fruit,setFruit]=useState(null);
+//   const [price,setPrice]=useState(null);
 
-  const onPress = () => {
-    fetch('http://10.0.2.2:8080/fruits', {
-      method: 'POST', 
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        "name": fruit,
-        "price": price
-      }),
+//   const onPress = () => {
+//     fetch('http://10.0.2.2:8080/fruits', {
+//       method: 'POST', 
+//       headers: {
+//         Accept: 'application/json',
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({
+//         "name": fruit,
+//         "price": price
+//       }),
       
-    })
-    Alert.alert("Fruta añadida correctamente")
-    .then((responseJson) => {
-      console.log('getting data from fectch', responseJson);
-      setFruit(null);
-      setPrice(null);
-    })
-      .catch(error => console.log(error));
-  }
+//     })
+//     .then((responseJson) => {
+//       console.log('getting data from fectch', responseJson);
+//       Alert.alert("Fruta añadida correctamente")
+//       setFruit(null);
+//       setPrice(null);
+//     })
+//     .catch(error => console.log(error));
+//   }
 
-  
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.informacion}> Agrega una fruta nueva </Text>
-      <View style={styles.busqueda}>
-        <Text style={styles.frutas}> Frutas </Text>
-        <ModalDropdown style={styles.select} textStyle={styles.opciones}
-          options={['Piña', 'Manzana','Melocoton', 'Uvas','Naranja','Kiwi','Platano','Pera']}
-          onSelect={(id, fruit) => setFruit(fruit)}/>
-      </View>
-      <View style={styles.busqueda}>
-        <Text style={styles.frutas}> Precio </Text>
-        <TextInput style={styles.input} onChangeText={price => setPrice(price)} />
-      </View>  
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <Text style={styles.informacion}> Agrega una fruta nueva </Text>
+//       <View style={styles.busqueda}>
+//         <Text style={styles.frutas}> Frutas </Text>
+//         <ModalDropdown style={styles.select} textStyle={styles.opciones}
+//           options={['Piña', 'Manzana','Melocoton', 'Uvas','Naranja','Kiwi','Platano','Pera']}
+//           onSelect={(id, fruit) => setFruit(fruit)}/>
+//       </View>
+//       <View style={styles.busqueda}>
+//         <Text style={styles.frutas}> Precio </Text>
+//         <TextInput style={styles.input} onChangeText={price => setPrice(price)} />
+//       </View>  
       
-      <Button title='Añadir' style={styles.informacion} onPress={onPress} />
+//       <Button title='Añadir' style={styles.informacion} onPress={onPress} />
 
-    </SafeAreaView>
-  );
-}
+//     </SafeAreaView>
+//   );
+// }
 
 function App() {
   return (
@@ -235,7 +149,7 @@ function App() {
     <NavigationContainer>
 
       <Tab.Navigator
-        screenOptions={({ route }) => ({
+        screenOptions={({ route }) => ({ 
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
             if (route.name === 'Listado') {
